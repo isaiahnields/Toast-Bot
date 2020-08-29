e126d50ad289b510c9d79c547bcf0df7d9b3ab90
Isaiah Nields <imnields@gmail.com>
2020-08-29 18:34:42 -0400

    Initial Commit

---
+++ Toast Bot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Toast Bot
+//
+//  Created by isaiahnields on 8/29/20.
+//  Copyright © 2020 com.isaiahnields. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
